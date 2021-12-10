7d5b1b4e63519c8a8a36573b518787a80f2399c4
janwillem.nijenhuis <j.w.nijenhuis@student.utwente.nl>
2021-12-10 14:29:01 +0100

    added Luhn until end

--- rust/luhn-trait/src/lib.rs
+++ rust/luhn-trait/src/lib.rs
@@ -7,8 +7,35 @@ pub trait Luhn {
 /// by hand for the every other type presented in the test suite,
 /// but your solution will fail if a new type is presented.
 /// Perhaps there exists a better solution for this problem?
-impl<'a> Luhn for &'a str {
+impl <T: ToString> Luhn for T {
     fn valid_luhn(&self) -> bool {
-        unimplemented!("Determine if '{}' is a valid credit card number.", self);
+        let temp = self.to_string();
+        let result = temp.trim();
+        if result.len() > 1 {
+            let mut sum = 0;
+            let mut even = false;
+            for c in result.chars().rev() {
+                if c.is_digit(10) || c.is_whitespace() {
+                    match c.to_digit(10) {
+                        None => {},
+                        Some(value) => {
+                            if even {
+                                let v = if value * 2 <= 9 { value * 2 } else { value * 2 - 9 };
+                                sum += v;
+                            } else {
+                                sum += value;
+                            }
+                            even = !even;
+                        }
+                    };
+                } else {
+                    return false;
+                }
+            }
+            if sum % 10 == 0 {
+                return true;
+            }
+        }
+        return false;
     }
 }
